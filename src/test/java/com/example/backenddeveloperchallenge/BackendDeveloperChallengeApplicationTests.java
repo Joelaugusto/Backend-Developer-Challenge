a4ed6b1424dc5ff629d3fcd0b5bf72b7fb56a58f
joel <joelaugusto>
2022-01-21 17:51:00 +0200

    feat: basic configuration

---
+++ src/test/java/com/example/backenddeveloperchallenge/BackendDeveloperChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddeveloperchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
